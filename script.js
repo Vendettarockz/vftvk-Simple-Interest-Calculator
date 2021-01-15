751e18caf3a51ea2fb890999307fe1ab7a6f3a5e
Vendettarockz <62636968+Vendettarockz@users.noreply.github.com>
2021-01-15 14:37:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ function compute()
     let r=document.getElementById("rate").value;
     let n=parseInt(document.getElementById("years").value);
     let y=parseInt(new Date().getFullYear());
+    if(p<1)
+    {
+    	alert("Enter a positive number.");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
     let i=(p*r*n)/100;
+    }
     document.getElementById("result").innerHTML=("If you deposit "+p+",<br>at an interest rate of "+r+"%.<br>You will recieve an amount of "+i+",<br>in the year "+(y+n));
 }
         
